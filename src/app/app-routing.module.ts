31b6ade56da3b9e07938a98cb68175e57469cb7d
Shuo Wang <swang342@usc.edu>
2020-11-01 03:29:10 -0800

    update dev

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {PostsComponent} from "./posts/posts.component";
-import {ContactComponent} from "./contact/contact.component";
+import {PostsComponent} from './posts/posts.component';
+import {ContactComponent} from './contact/contact.component';
+import {HomepageComponent} from './homepage/homepage.component';
+import {WatchlistComponent} from './watchlist/watchlist.component';
+import {PortfolioComponent} from './portfolio/portfolio.component';
+import {DetailsComponent} from './details/details.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'posts',
-    pathMatch: 'full'
+    component: HomepageComponent
+  },
+  {
+    path: 'watchlist',
+    component: WatchlistComponent
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioComponent
+  },
+  {
+    path: 'details',
+    component: DetailsComponent
   },
   {
     path: 'posts',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
 import { HttpClientModule } from "@angular/common/http";
 import { ContactComponent } from './contact/contact.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { WatchlistComponent } from './watchlist/watchlist.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostsComponent,
-    ContactComponent
+    ContactComponent,
+    HomepageComponent,
+    WatchlistComponent,
+    PortfolioComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
